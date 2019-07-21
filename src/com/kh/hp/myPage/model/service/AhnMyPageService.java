8eb2be2d4d4860f310abab4b04edc253f267d611
j8911 <j8911@LAPTOP-T93QK7TL>
2019-07-22 08:53:04 +0900

    20190722 AM08:52

--- src/com/kh/hp/myPage/model/service/AhnMyPageService.java
+++ src/com/kh/hp/myPage/model/service/AhnMyPageService.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import com.kh.hp.myPage.model.dao.AhnMyPageDao;
 import com.kh.hp.myPage.model.vo.AhnLevelupVO;
 import com.kh.hp.myPage.model.vo.AhnMyPageVO;
+import com.kh.hp.myPage.model.vo.AhnUsingInfoVO;
 
 import static com.kh.hp.common.JDBCTemplate.*;
 
@@ -23,6 +24,14 @@ public class AhnMyPageService {
 	public AhnLevelupVO updateLevelOne(int levelUpInfo) {
 		Connection con = getConnection();
 		AhnLevelupVO responseUserVO = new AhnMyPageDao().updateLevelOne(con, levelUpInfo);
+		close(con);
+		
+		return responseUserVO;
+	}
+
+	public AhnUsingInfoVO updateUsingOne(int usingInfo) {
+		Connection con = getConnection();
+		AhnUsingInfoVO responseUserVO = new AhnMyPageDao().updateUsingOne(con, usingInfo);
 		close(con);
 		
 		return responseUserVO;
